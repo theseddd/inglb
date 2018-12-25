7fb7bc16ff207fa13adce65f69cb0a391e5d6778
theseddd <theseddd@gmail.com>
2018-12-26 02:42:28 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,7 +16,12 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['source']['userId'];
+			$text = "You is "
+			$text =+ $event['source']['displayName'];
+			$text =+ " UserId:"
+			$text =+ $event['source']['userId'];
+			
+			"displayName":"Brown"
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
