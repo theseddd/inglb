e948e6190f574b03157992abb875a7d641f1817e
theseddd <theseddd@gmail.com>
2018-12-27 15:57:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
+date_default_timezone_set("Asia/Bangkok");
+$timestamp = str(date("d/m/Y h:i:s"));
+$date = str(date("d/m/Y"));
+$time = str(date("h:i:s"));
+
+echo "DateTime".$timestamp;
 echo "Hello LINE BOT";
