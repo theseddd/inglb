f868be4a56f9c167a6f2cb7fa5b22bc7d54ff0f1
theseddd <theseddd@gmail.com>
2018-12-27 17:17:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,9 @@ $apikey = 'AIzaSyAgSEnkENLqDB2Udps_qKvXcv7HwRQUsrE';
 $range_Y = strval(date("Y"));
 $range_M = strval(date("m"));
 
-$header = array('Content-Type: application/json');
 
+//////////////////////////////////////////////////////////////////////////////////////
+$header = array('Content-Type: application/json');
 //$url1 = 'https://sheets.googleapis.com/v4/spreadsheets/'.$spreadsheetId.'/values/'.$range_Y.'%2F'.$range_M.'12?valueRenderOption=FORMATTED_VALUE&key='.$spreadsheetId;
 $url1 = 'https://sheets.googleapis.com/v4/spreadsheets/'.$spreadsheetId.'/values/'.$range_Y.'%2F'.$range_M.'?valueRenderOption=FORMATTED_VALUE&key='.$apikey;
 $ch1 = curl_init($url1);
@@ -21,11 +22,34 @@ $response = curl_exec($ch1);
 $json_array = json_decode($response, true);
 $username = $events2['displayName'];
 curl_close($ch1);
-
-print_r(array_values($json_array));
+//print_r(array_values($json_array));
 $elementCount  = count($json_array['values']);
+///////////////////////////////////////////////////////////////////////////////////////
 
-
+///////////////////////////////////////////////////////////////////////////////////////
+$header = array('Content-Type: application/json', 'Authorization: Bearer ' . $a_token);
+	// Build message to reply back
+$message = [
+	'type' => 'text',
+	'text' => $ms
+];
+			
+// Make a POST Request to Messaging API to reply to sender
+$url2 = 'https://sheets.googleapis.com/v4/spreadsheets/'.$spreadsheetId.'/values/'.$range_Y.'%2F'.$range_M.':append?valueInputOption=RAW&key='.$apikey;
+$data2 = [
+	'replyToken' => $rp_token,
+	'messages' => [$message],
+	];
+$post2 = json_encode($data2);
+$ch2 = curl_init($url2);
+curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $post2);
+curl_setopt($ch2, CURLOPT_HTTPHEADER, $header);
+curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch2);
+curl_close($ch2);
+///////////////////////////////////////////////////////////////////////////////////////
 
 echo "U".$elementCount;
 echo "DateTime".$timestamp;
