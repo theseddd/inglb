d877ff79e83578f6780e8b3a4c3b75bfd90020f7
theseddd <theseddd@gmail.com>
2018-12-27 16:24:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,12 @@ $timestamp = date("d/m/Y h:i:s");
 $date = date("d/m/Y");
 $time = date("h:i:s");
 $spreadsheetId = "1XJ7j_zMc-2tBKoEx0RAMnljCwdOfNacGQP8i2K-jM30";
+$apikey = "AIzaSyAgSEnkENLqDB2Udps_qKvXcv7HwRQUsrE";
 $range_Y = date("Y");
 $range_M = date("m");
 
 $header = array('Content-Type: application/json');
-$url1 = 'https://sheets.googleapis.com/v4/spreadsheets/1XJ7j_zMc-2tBKoEx0RAMnljCwdOfNacGQP8i2K-jM30/values/'.$range_Y.'%2F'.$range_M.'12?key='.$spreadsheetId;
+$url1 = 'https://sheets.googleapis.com/v4/spreadsheets/'.$spreadsheetId.'/values/'.$range_Y.'%2F'.$range_M.'12?key='.$spreadsheetId;
 $ch1 = curl_init($url1);
 curl_setopt($ch1, CURLOPT_CUSTOMREQUEST, "GET");
 curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
