5d98164a9b50c1616db1fa50d0cf5a87563b13ce
theseddd <theseddd@gmail.com>
2018-12-28 18:00:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -37,19 +37,19 @@ $url2 = 'https://sheets.googleapis.com/v4/spreadsheets/'.$spreadsheetId.'/values
 //$url2 = 'https://sheets.googleapis.com/v4/spreadsheets/1XJ7j_zMc-2tBKoEx0RAMnljCwdOfNacGQP8i2K-jM30/values/2018%2F12:append?valueInputOption=RAW&key=AIzaSyAgSEnkENLqDB2Udps_qKvXcv7HwRQUsrE';
 $post2 = json_encode($data2);
 echo $post2;
-$post2 = '{}';
-$ch2 = curl_init($url2);
-curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch2, CURLOPT_POSTFIELDS, $post2);
-curl_setopt($ch2, CURLOPT_HTTPHEADER, $header);
-curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch2);
-$json_array = json_decode($result, true);
+//$post2 = '{}';
+//$ch2 = curl_init($url2);
+//curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, "POST");
+//curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+//curl_setopt($ch2, CURLOPT_POSTFIELDS, $post2);
+//curl_setopt($ch2, CURLOPT_HTTPHEADER, $header);
+//curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 1);
+//$result = curl_exec($ch2);
+//$json_array = json_decode($result, true);
 
-curl_close($ch2);
+//curl_close($ch2);
 ///////////////////////////////////////////////////////////////////////////////////////
-print_r(array_values($json_array));
+//print_r(array_values($json_array));
 echo "U".$elementCount;
 echo "DateTime".$timestamp;
 echo "Hello LINE BOT";
