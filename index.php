4ab9ae3d08b849595c39bf9f6e68a91628a0ce4a
theseddd <theseddd@gmail.com>
2018-12-28 22:20:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,33 +27,27 @@ $range_M = strval(date("m"));
 ///////////////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////////////
-$header = array('Content-Type: application/json');
-	// Build message to reply back
-$message1 = ['1','1','1'];
-$message2 = [$message1];
-$data2 = [
-		'range' => [$message],
-	];
-		
-
 // Make a POST Request to Messaging API to reply to sender
-$url2 = 'https://sheets.googleapis.com/v4/spreadsheets/'.$spreadsheetId.'/values/'.$range_Y.'%2F'.$range_M.':append?valueInputOption=RAW&key='.$apikey;
+$url = 'https://sheets.googleapis.com/v4/spreadsheets/'.$spreadsheetId.'/values/'.$range_Y.'%2F'.$range_M.':append?valueInputOption=RAW&key='.$apikey;
 //$url2 = 'https://sheets.googleapis.com/v4/spreadsheets/1XJ7j_zMc-2tBKoEx0RAMnljCwdOfNacGQP8i2K-jM30/values/2018%2F12:append?valueInputOption=RAW&key=AIzaSyAgSEnkENLqDB2Udps_qKvXcv7HwRQUsrE';
-$post2 = json_encode($data2);
-echo $post2;
-//$post2 = '{}';
-$ch2 = curl_init($url2);
-curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch2, CURLOPT_POSTFIELDS, $post2);
-curl_setopt($ch2, CURLOPT_HTTPHEADER, $header);
-curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch2);
+//create a new cURL resource
+$ch = curl_init($url);
+//setup request to send json via POST
+$data = array(array('1','2','3'););
+$payload = json_encode(array("range" => $data));
+//attach encoded JSON string to the POST fields
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+//set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+//return response instead of outputting
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//execute the POST request
+$result = curl_exec($ch);
+//close cURL resource
+curl_close($ch);
 $json_array = json_decode($result, true);
-
-curl_close($ch2);
 ///////////////////////////////////////////////////////////////////////////////////////
-//print_r(array_values($json_array));
+print_r(array_values($json_array));
 echo "U".$elementCount;
 echo "DateTime".$timestamp;
 echo "Hello LINE BOT";
