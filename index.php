45acfd033380dd2086021617984f2970ac26eadd
theseddd <theseddd@gmail.com>
2018-12-28 17:46:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -36,8 +36,8 @@ $data2 = [
 		
 
 // Make a POST Request to Messaging API to reply to sender
-$url2 = 'https://sheets.googleapis.com/v4/spreadsheets/'.$spreadsheetId.'/values/'.$range_Y.'%2F'.$range_M.':append?valueInputOption=RAW&key='.$apikey;
-
+//$url2 = 'https://sheets.googleapis.com/v4/spreadsheets/'.$spreadsheetId.'/values/'.$range_Y.'%2F'.$range_M.':append?valueInputOption=RAW&key='.$apikey;
+$url2 = 'https://sheets.googleapis.com/v4/spreadsheets/1XJ7j_zMc-2tBKoEx0RAMnljCwdOfNacGQP8i2K-jM30/values/2018%2F12:append?valueInputOption=RAW&key=AIzaSyAgSEnkENLqDB2Udps_qKvXcv7HwRQUsrE';
 $post2 = json_encode($data2);
 echo $post2;
 $ch2 = curl_init($url2);
@@ -47,11 +47,11 @@ curl_setopt($ch2, CURLOPT_POSTFIELDS, $post2);
 curl_setopt($ch2, CURLOPT_HTTPHEADER, $header);
 curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch2);
-curl_close($ch2);
 $json_array = json_decode($result, true);
-print_r(array_values($json_array));
-///////////////////////////////////////////////////////////////////////////////////////
 
+curl_close($ch2);
+///////////////////////////////////////////////////////////////////////////////////////
+print_r(array_values($json_array));
 echo "U".$elementCount;
 echo "DateTime".$timestamp;
 echo "Hello LINE BOT";
