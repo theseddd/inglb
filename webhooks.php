9fad86efdb137bfe6dc7fab0bfd92724400c6ade
theseddd <theseddd@gmail.com>
2019-03-19 08:08:13 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,6 +1,6 @@
 <?php // callback.php
 
-require "vendor/autoload.php";
+//require "vendor/autoload.php";
 //require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 date_default_timezone_set("Asia/Bangkok");
 $timestamp = str(date("d/m/Y h:i:s"));
